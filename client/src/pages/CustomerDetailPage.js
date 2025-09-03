6f8b2eb6b6326d4a8297724933fd095ef0c54074
Manoj <122385222+Manu2919@users.noreply.github.com>
2025-09-03 17:24:49 +0530

    Update CustomerDetailPage.js

--- client/src/pages/CustomerDetailPage.js
+++ client/src/pages/CustomerDetailPage.js
@@ -1,3 +1,5 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useParams, useNavigate, Link } from 'react-router-dom';
@@ -122,5 +124,6 @@ const CustomerDetailPage = () => {
 
 
 export default CustomerDetailPage;
+
 
 
